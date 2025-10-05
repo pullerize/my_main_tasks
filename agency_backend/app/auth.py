f29200ea1f3eddfdd1800c8e48e31046a4799760
pullerize <pullerize@gmail.com>
2025-10-05 17:11:48 +0500

    Fix: bcrypt 72 byte password limit

--- agency_backend/app/auth.py
+++ agency_backend/app/auth.py
@@ -44,6 +44,9 @@ def verify_password(plain_password, hashed_password):
 
 
 def get_password_hash(password):
+    # bcrypt has 72 byte limit - truncate if necessary
+    if len(password.encode('utf-8')) > 72:
+        password = password[:72]
     return pwd_context.hash(password)
 
 
